9bc8e7fe9552386b79a5b7dedd5091e8e75d011d
Merhyi <157216055+Merhyi@users.noreply.github.com>
2025-03-01 21:46:49 +0800

    Add files via upload

--- vue_frontend/src/main.js
+++ vue_frontend/src/main.js
@@ -13,10 +13,17 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 import router from "./router"
+import CustomScrollbar from 'custom-vue-scrollbar';
+import 'custom-vue-scrollbar/dist/style.css';
+
+
 
 const app = createApp(App)
 
 registerPlugins(app)
 
 // app.mount('#app')
+app.component(CustomScrollbar.name, CustomScrollbar);
+
+
 app.use(router).mount('#app')
